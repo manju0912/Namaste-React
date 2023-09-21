f10bdbc7c90f9cbf5630b53c5b8364cf37da04d1
Manju Gurung <manju.gurung0912@gmail.com>
2023-09-21 12:45:57 +0530

    Namaste React Assignment 01 Uploaded

---
+++ Assignment-01-Inception/App.js
@@ -0,0 +1,23 @@
+// Nested Elements in React
+// <div id="parent">
+//     <div id="child">
+//         <h1></h1>
+//     </div>
+// </div>
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    React.createElement("div", {id: "child"},
+    [React.createElement("h1", {}, "I'm an h1 tag"), React.createElement("h2", {}, "I'm an h2 tag")])
+);
+
+const heading = React.createElement("h1", {}, "Hello World from React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+root.render(parent);
+
+
+//Note To Take
+// React Element is an Object.
